27ecb3c98174bc84f184bec39999a02e289e06d3
opornik-B-plenta <147750628+opornik-B-plenta@users.noreply.github.com>
2023-12-07 01:12:30 +0300

    Delete сайт directory

--- сайт/registred.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Проверка, авторизован ли пользователь
-    const loggedIn = localStorage.getItem("loggedIn");
-    if (!loggedIn || loggedIn !== "true") {
-        // Если пользователь не авторизован, показываем сообщение и перенаправляем его на страницу авторизации
-        alert("Пользователь не авторизован.");
-        window.location.href = "hw2.html";
-    }
-});
